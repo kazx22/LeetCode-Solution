7a6d53f44a8963434132d2c48c727450729d27db
kazx22 <kazialifhaider@gmail.com>
2023-04-17 15:46:15 +0600

    3rd

--- app.ts
+++
@@ -1,8 +0,0 @@
-function kidsWithCandies(candies: number[], extraCandies: number): boolean[] {
-  let result: boolean[] = [];
-  let max = Math.max(...candies);
-  candies.forEach((val) => {
-    val + extraCandies >= max ? result.push(true) : result.push(false);
-  });
-  return result;
-}
